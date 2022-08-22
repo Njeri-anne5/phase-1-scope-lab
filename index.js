3a5e7d97901f10ae0d522c686e91381314851c28
+ <+>
2022-08-22 19:38:09 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Write your solution in this file!
+var customerName="bob"
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase();
+    console.log (customerName);
+}
+
+
+var setBestCustomer;
+function setBestCustomer(){
+    
+    return bestCustomer="not bob";
+    
+}
+
+
+
+function overwriteBestCustomer(){
+    var setBestCustomer="maybe bob"
+    return setBestCustomer;
+}//Not passing,avoiding hardcoding the parameter, logic, when var is used to declare a variable, 
+//its inconsistent, it overwrites its vaues.
+
+/*function overwriteBestCustomer(setBestCustomer="maybe bob"){
+    return setBestCustomer;
+}passes*/
+
+
+
+function changeLeastFavoriteCustomer(){
+    const changeLeastFavoriteCustomer="Tess";
+    changeLeastFavoriteCustomer="Rachael";
+    return changeLeastFavoriteCustomer;
+}
+
